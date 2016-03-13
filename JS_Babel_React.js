d22736ef71a800f9d82308332a8d4e2fbbedc546
Drew Sartorius <duckydesign@gmail.com>
2016-03-13 17:31:41 -0400

    Update schema and data format

--- JS_Babel_React.js
+++ JS_Babel_React.js
@@ -1,22 +1,37 @@
+var data = [{
+                    num: "1.",
+                    img: "http://www.mamadomia.org/img/smile-top-img.png",
+                    username: "Michael Jackson",
+                    recent: "35",
+                    alltime: "104"
+                  },
+                  {
+                    num: "2.",
+                    img: "http://rs304.pbsrc.com/albums/nn180/4chanRus/Awesome%20Face/1213841779552.png~c200",
+                    username: "Debbie Downer",
+                    recent: "20",
+                    alltime: "208"
+                 }];
+
 var UserRows = React.createClass({
   render: function(){
-    var rows = this.props.data.map(function(row){
+    var rows = this.props.data.map(function(row,index){
       return (
         <tr className="example">
           <td className="num">
-            {row.num}
+            {index+1}
           </td>
           <td className="pic">
             <img src={row.img} />
           </td>
           <td className="name">
-            {row.name}
+            {row.username}
           </td>
           <td className="monthPoints">
-            {row.monthly}
+            {row.recent}
           </td>
           <td className="totalPoints">
-            {row.total}
+            {row.alltime}
           </td>
         </tr>
       );
@@ -30,29 +45,13 @@ var UserRows = React.createClass({
 });
 
 var Leaderboard = React.createClass({
-  getInitialState: function(){
-    return {data:[{
-                    num: "1.",
-                    img: "http://www.mamadomia.org/img/smile-top-img.png",
-                    name: "Michael Jackson",
-                    monthly: "35",
-                    total: "104"
-                  },
-                  {
-                    num: "2.",
-                    img: "http://rs304.pbsrc.com/albums/nn180/4chanRus/Awesome%20Face/1213841779552.png~c200",
-                    name: "Debbie Downer",
-                    monthly: "20",
-                    total: "208"
-                 }]};
-  },
   render: function() {
     return (
       <div className="leaderContent">  
-        <UserRows data={this.state.data} />
+        <UserRows data={this.props.data} />
       </div>
     );
   }
 });
 
-React.render(<Leaderboard />,document.getElementById("content"));
+React.render(<Leaderboard data={data} />,document.getElementById("content"));
